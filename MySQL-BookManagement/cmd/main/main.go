ef650fdf5273474318ea9b1917505f5d45465ce6
Dawid <56120592+dawiddzhafarov@users.noreply.github.com>
2022-06-01 19:40:01 +0200

    Add files via upload

---
+++ MySQL-BookManagement/cmd/main/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/dawiddzhafarov/GoProjects/MySQL-BookStoreManagement/pkg/routes"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
+}

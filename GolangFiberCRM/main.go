95fb5a0a5ba2ef1affdd895bc1973805f617159b
dawiddzhafarov <dawid.dzhafarov@gmail.com>
2022-06-04 17:55:06 +0200

    files upload

--- GolangFiberCRM/main.go
+++ GolangFiberCRM/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"github.com/gofiber/fiber"
+	"github.com/dawiddzhafarov/GoProjects/GolangFiberCRM/database"
+)
 
+func main() {
+	app := fiber.New()
+	setupRoutes(app)
+	app.Listen(3000)
+	initDatabase()
+	defer database.DBconn
+}
+
+func initDatabase() {
+	database.
+}
+
+func setupRoutes(app *fiber.App) {
+	app.Get(GetLead)
+	app.Get(GetLeads)
+	app.Post(NewLead)
+	app.Delete(DeleteLead)
 }

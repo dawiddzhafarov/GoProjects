eb0127e27f9f2e1d9f09625483261e8889ae95d3
dawiddzhafarov <dawid.dzhafarov@gmail.com>
2022-06-16 17:04:28 +0200

    arrays, slices finished

--- GoBasics/arrays_slices/sum.go
+++ GoBasics/arrays_slices/sum.go
@@ -7,3 +7,25 @@ func Sum(values []int) int {
 	}
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+func SumAllTails(slices ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range slices {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			sums = append(sums, Sum(numbers[1:]))
+		}
+
+	}
+	return sums
+}

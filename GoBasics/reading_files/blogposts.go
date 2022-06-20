81c1df85cf03c3397872106b6729df15db867048
dawiddzhafarov <dawid.dzhafarov@gmail.com>
2022-06-20 21:47:49 +0200

    reading and parsing files, create wrong format handling

--- GoBasics/reading_files/blogposts.go
+++ GoBasics/reading_files/blogposts.go
@@ -1,13 +1,26 @@
 package blogposts
 
 import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
 	"io/fs"
+	"strings"
 )
 
 type Post struct {
-	Title, Description, Body string
-	Tags                     []string
+	Title, Description string
+	Tags               []string
+	Body               string
 }
+
+const (
+	titleSeparator       = "Title: "
+	descriptionSeparator = "Description: "
+	tagsSeparator        = "Tags: "
+)
 
 func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 	dir, err := fs.ReadDir(fileSystem, ".")
@@ -16,8 +29,50 @@ func NewPostsFromFS(fileSystem fs.FS) ([]Post, error) {
 		return nil, err
 	}
 	var posts []Post
-	for range dir {
-		posts = append(posts, Post{})
+
+	for _, f := range dir {
+		if !strings.HasSuffix(f.Name(), ".md") {
+			return nil, errors.New("File not supported")
+		}
+		post, err := getPost(fileSystem, f.Name())
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, post)
 	}
 	return posts, nil
+}
+
+func getPost(fileSystem fs.FS, fileName string) (Post, error) {
+	postFile, err := fileSystem.Open(fileName)
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+	return newPost(postFile)
+}
+
+func newPost(postBody io.Reader) (Post, error) {
+	scanner := bufio.NewScanner(postBody)
+
+	readMetaLine := func(tagName string) string {
+		scanner.Scan()
+		return strings.TrimPrefix(scanner.Text(), tagName)
+	}
+
+	return Post{
+		Title:       readMetaLine(titleSeparator),
+		Description: readMetaLine(descriptionSeparator),
+		Tags:        strings.Split(readMetaLine(tagsSeparator), ", "),
+		Body:        readBody(scanner),
+	}, nil
+}
+
+func readBody(scanner *bufio.Scanner) string {
+	scanner.Scan()
+	buf := bytes.Buffer{}
+	for scanner.Scan() {
+		fmt.Fprintln(&buf, scanner.Text())
+	}
+	return strings.TrimSuffix(buf.String(), "\n")
 }
--- GoBasics/reading_files/blogposts_test.go
+++ GoBasics/reading_files/blogposts_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	blogposts "github.com/dawiddzhafarov/GoProjects/GoBasics/reading_files"
 	"io/fs"
+	"reflect"
 	"testing"
 	"testing/fstest"
 )
@@ -11,11 +12,67 @@ import (
 type StubFailingFS struct {
 }
 
+const (
+	firstBody = `Title: Post 1
+Description: Description 1
+Tags: tdd, go
+---
+Hello
+World`
+	secondBody = `Title: Post 2
+Description: Description 2
+Tags: tdd2, go2
+---
+L
+O
+L`
+)
+
 func (s StubFailingFS) Open(name string) (fs.File, error) {
 	return nil, errors.New("Oh no, I always fail")
 }
 
 func TestNewBlogPosts(t *testing.T) {
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
+	}
+	posts, err := blogposts.NewPostsFromFS(fs)
+	if err != nil {
+		t.Errorf("error occured but shouldnt")
+	}
+	got := posts[0]
+	want := blogposts.Post{
+		Title:       "Post 1",
+		Description: "Description 1",
+		Tags:        []string{"tdd", "go"},
+		Body: `Hello
+World`,
+	}
+	assertPost(t, got, want)
+}
+
+func TestWrongFormat(t *testing.T) {
+	fs := fstest.MapFS{
+		"hello world.md":  {Data: []byte(firstBody)},
+		"hello world.txt": {Data: []byte(firstBody)},
+	}
+	posts, err := blogposts.NewPostsFromFS(fs)
+	if err == nil {
+		t.Errorf("Should get wrong file format error")
+	}
+	got := posts[1]
+	want := blogposts.Post{
+		Title:       "Post 1",
+		Description: "Description 1",
+		Tags:        []string{"tdd", "go"},
+		Body: `Hello
+World`,
+	}
+	assertPost(t, got, want)
+}
+
+func TestReadFileError(t *testing.T) {
 	t.Run("always failing execution", func(t *testing.T) {
 		_, err := blogposts.NewPostsFromFS(StubFailingFS{})
 
@@ -23,20 +80,11 @@ func TestNewBlogPosts(t *testing.T) {
 			t.Errorf("Error should occur, but didn't")
 		}
 	})
-	t.Run("num of posts test", func(t *testing.T) {
-		fs := fstest.MapFS{
-			"hello hello world.md": {Data: []byte("hi")},
-			"hello-world2.md":      {Data: []byte("hola")},
-		}
+}
 
-		posts, err := blogposts.NewPostsFromFS(fs)
-
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		if len(posts) != len(fs) {
-			t.Errorf("got %d posts, but wanted %d posts", len(posts), len(fs))
-		}
-	})
+func assertPost(t *testing.T, got blogposts.Post, want blogposts.Post) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, but wanted %+v", got, want)
+	}
 }

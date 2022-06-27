7a02cd9a9b5f384762fee0303c7fd687287d3d5f
dawiddzhafarov <dawid.dzhafarov@gmail.com>
2022-06-27 12:22:28 +0200

    serving html to record a winner on a webserver

--- HTTPapplication/server_test.go
+++ HTTPapplication/server_test.go
@@ -2,11 +2,14 @@ package poker
 
 import (
 	"fmt"
+	"github.com/gorilla/websocket"
 	"io"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
+	"strings"
 	"testing"
+	"time"
 )
 
 func newGetScoreRequest(name string) *http.Request {
@@ -143,4 +146,42 @@ func getLeagueFromResponse(t *testing.T, body io.Reader) []Player {
 		t.Fatalf("Unable to parse response from server %q into slice of Player, '%v'", body, err)
 	}
 	return league
+}
+
+func TestGame(t *testing.T) {
+	t.Run("GET /game return 200", func(t *testing.T) {
+		server := NewPlayerServer(&StubPlayerStore{})
+
+		request := newGameRequest()
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusOK)
+	})
+	t.Run("when we get a message over a websocket it is a winner of a game", func(t *testing.T) {
+		store := &StubPlayerStore{}
+		winner := "Ruth"
+		server := httptest.NewServer(NewPlayerServer(store))
+		defer server.Close()
+
+		wsURL := "ws" + strings.TrimPrefix(server.URL, "http") + "/ws"
+
+		ws, _, err := websocket.DefaultDialer.Dial(wsURL, nil)
+		if err != nil {
+			t.Fatalf("could not open a ws connection on %s %v", wsURL, err)
+		}
+		defer ws.Close()
+
+		if err := ws.WriteMessage(websocket.TextMessage, []byte(winner)); err != nil {
+			t.Fatalf("Could not send message over ws connection %v", err)
+		}
+		time.Sleep(10 * time.Millisecond)
+		AssertPlayerWin(t, store, winner)
+	})
+}
+
+func newGameRequest() *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, "game", nil)
+	return req
 }
